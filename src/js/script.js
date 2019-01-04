ac6ef22acfa310ca61f7b6d6fa5e40242fb2c085
Myroslav <myroslavlutsiv@gmail.com>
2019-01-04 12:42:11 +0200

    [BUGFIX] remove ES6 stuff

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
-$(window).on('load', () => {
+$(window).on('load', function () {
     App.init()
 })
 
-const App = {
+var App = {
 
-    init() {
+    init: function () {
         this.isTouch()
         this.heroSlider()
         this.mobileNav()
@@ -16,14 +16,14 @@ const App = {
         this.pageScroll()
     },
 
-    collapsibleItem() {
-        let $item = $('.js__item-target')
+    collapsibleItem: function () {
+        var $item = $('.js__item-target')
         if ($item.length) {
             $item.each(function () {
                $(this).on('click', function () {
                    if ($(window).width() < 768) {
                         $(this).toggleClass('_active')
-                        let $parent = $(this).parents().eq(1);
+                        var $parent = $(this).parents().eq(1);
                         $parent.find('.service-item__img-preview').stop().slideToggle(300)
                         $parent.find('.service-item__caption-body').stop().slideToggle(300)
                    }
@@ -32,8 +32,8 @@ const App = {
         }
     },
 
-    pageScroll() {
-        const $nav = $('.js__nav-list')
+    pageScroll: function () {
+        var $nav = $('.js__nav-list')
         if ($nav.length) {
             $nav.find('a').pageNav({
                 'active_item' : '_active',
@@ -42,24 +42,23 @@ const App = {
         }
     },
 
-    parallaxImage() {
+    parallaxImage: function() {
         if ($('.js__parallax-image').length) {
             paraxify('.js__parallax-image')
         }
     },
 
-    teamFilter() {
-        let $switcher = $('.js__team')
-        let $item = $('.team-item')
-
-        const filter = (href) => {
+    teamFilter: function() {
+        var $switcher = $('.js__team')
+        var $item = $('.team-item')
+        var filter = function (href) {
             $item.each(function () {
                 $(this).attr('data-team') === href ? $(this).addClass('_active') : $(this).removeClass('_active')
             })
         }
         if ($switcher.length) {
             filter('team-1')
-            $switcher.on('click', (e) => {
+            $switcher.on('click', function (e) {
                 e.preventDefault()
                 $(e.target).addClass('_active').siblings().removeClass('_active')
                 filter($(e.target).attr('href'))
@@ -67,14 +66,14 @@ const App = {
         }
     },
 
-    aboutUsToggler() {
-        let $switcher = $('.js__about-us')
-        let $description = $('.js__about-us__description')
-        let $gallery = $('.js__about-us__allery-spot')
-        let $sl = $('.js__gallery')
+    aboutUsToggler: function () {
+        var $switcher = $('.js__about-us')
+        var $description = $('.js__about-us__description')
+        var $gallery = $('.js__about-us__allery-spot')
+        var $sl = $('.js__gallery')
 
         if ($switcher.length && $description.length && $gallery.length && $sl.length) {
-            let slInstance = []
+            var slInstance = []
             $sl.each(function () {
                 slInstance.push(new Swiper(this, {
                     navigation: {
@@ -84,7 +83,7 @@ const App = {
                     autoHeight: true
                 }))
             })
-            $switcher.on('click', (e) => {
+            $switcher.on('click', function (e) {
                 e.preventDefault()
                 $(e.target).addClass('_active').siblings().removeClass('_active')
                 $description.find($(e.target).attr('href')).addClass('_active').siblings().removeClass('_active')
@@ -96,34 +95,34 @@ const App = {
         }
     },
 
-    isTouch() {
+    isTouch: function () {
         ('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0) ?
         $('html').addClass('touch') : $('html').addClass('no-touch');
     },
 
-    mobileNav() {
-        let $target = $('.header-nav__btn')
-        $target.on('click', () => {
+    mobileNav: function () {
+        var $target = $('.header-nav__btn')
+        $target.on('click', function () {
             $('html').toggleClass('_nav-open')
         })
     },
 
-    map() {
-        let $check = $('.js__check-item')
-        let uluru = JSON.parse($check[0].value)
-        let map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: uluru})
-        let marker = new google.maps.Marker({position: uluru, map: map})
+    map: function () {
+        var $check = $('.js__check-item')
+        var uluru = JSON.parse($check[0].value)
+        var map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: uluru})
+        var marker = new google.maps.Marker({position: uluru, map: map})
 
         $check.on('change', function () {
-            let uluru = JSON.parse(this.value)
+            var uluru = JSON.parse(this.value)
             marker.setPosition(uluru)
             map.setCenter(uluru)
         })
     },
 
-    heroSlider() {
-        let elem = document.querySelector('.js__hero-slider')
-        new Swiper(elem, {
+    heroSlider: function() {
+        var elem = document.querySelector('.js__hero-slider')
+        var sw = new Swiper(elem, {
             autoplay: {
                 delay: 5000,
             },
